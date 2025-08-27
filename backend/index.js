67c962ae65166e8dcfac407a80bf1b1503ac287d
Tanvi Sudhakar Sandbhor <149931978+tanvisandbhor@users.noreply.github.com>
2025-08-27 23:55:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
-// index.js
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const connectToMongo = require('./db');
 const { models } = require('mongoose');
-
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,17 +11,9 @@ const PORT = process.env.PORT || 5000;
 connectToMongo();
 
 // Middleware
-app.use(cors());
-
-app.use((req,res,next)=>{
-  res.setHeader("Access-control-Allow-Origin","http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin ,x-Requested-With,Content-Type,Accept"
-  );
-  next();
-})
-// Sample route
+app.use(cors({
+  origin: ["http://localhost:3000", "https://foodyupp-2b.vercel.app"]
+}));
 
 app.use(express.json());
 app.use('/api', require('./Routes/CreateUser'));
@@ -31,12 +21,9 @@ app.use('/api', require('./Routes/DisplayData'));
 app.use('/api', require('./Routes/myorderData'));
 app.use('/api/payment', require('./Routes/payment'));
 
-
-
 app.get('/', (req, res) => {
   res.send('🚀 Hello from backend, DB connected!');
 });
-
 
 // Start server
 app.listen(PORT, () => {
